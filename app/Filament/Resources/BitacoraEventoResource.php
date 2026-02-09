4b5815703c1165a01294e8311fb7e0769e3a9650
Steven Rivera <steven.melendez001@gmail.com>
2026-02-08 18:29:00 -0600

    Cambios en la vista BitacoraEvento3

--- app/Filament/Resources/BitacoraEventoResource.php
+++ app/Filament/Resources/BitacoraEventoResource.php
@@ -39,7 +39,7 @@ class BitacoraEventoResource extends Resource
     {
         return $form->schema([
         Forms\Components\DateTimePicker::make('created_at')->label('Fecha')->disabled(),
-        Forms\Components\TextInput::make('usuario.name')->label('Usuario')->disabled(),
+        Forms\Components\TextInput::make('solicitante_id')->label('Usuario')->disabled(),
         Forms\Components\TextInput::make('accion')->disabled(),
         Forms\Components\TextInput::make('entidad_tipo')->disabled(),
         Forms\Components\TextInput::make('entidad_id')->disabled(),
