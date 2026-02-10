2182cc6db05676b03187b35708394fe53589a76f
Steven Rivera <steven.melendez001@gmail.com>
2026-02-10 10:08:45 -0600

    Roles Y UserResource3

--- storage/framework/views/d648e335785847da2e2f69ef8ef748d8.php
+++
@@ -1,6 +0,0 @@
-
-                <div class="text-center text-xs text-gray-500 mt-6">
-                    <p>© <?php echo e(date("Y")); ?> Asamblea Legislativa de El Salvador.</p>
-                    <p>Todos los derechos reservados.</p>
-                </div>
-            <?php /**PATH C:\Users\steve\transporte-asamblea01\storage\framework\views/b92cc82d605a68fc32eb4eec096a6403.blade.php ENDPATH**/ ?>
