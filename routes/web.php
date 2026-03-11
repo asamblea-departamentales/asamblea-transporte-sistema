f5003198f76f01ad8bda24605643fb7c6a15427d
Steven Rivera <steven.melendez001@gmail.com>
2026-03-11 12:19:36 -0600

    add: Reporte Flota Vehicular7

--- routes/web.php
+++ routes/web.php
@@ -193,5 +193,5 @@ Route::get('/reportes/solicitudes-mantenimiento/excel', function (Request $reque
 })->name('reportes.solicitudes-mantenimiento.excel');
 
 // ---------------------- REPORTES -------------------------------------------------------------------------------------------------------//
-Route::get('/reportes/flota-vehicular/pdf', [ReporteFlotaVehicularController::class, 'pdf'])
-    ->name('reportes.flota-vehicular.pdf');
+Route::get('/reports/flota-vehicular/pdf', [ReporteFlotaVehicularController::class, 'pdf'])
+    ->name('reports.flota-vehicular.pdf');
