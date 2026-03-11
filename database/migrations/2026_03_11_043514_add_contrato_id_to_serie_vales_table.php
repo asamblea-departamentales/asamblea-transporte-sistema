2f58d1eaa5a68efe0b42a6393ecd3308683a1b03
Steven Rivera <steven.melendez001@gmail.com>
2026-03-10 22:35:43 -0600

    add: Vales para combustible2

---
+++ database/migrations/2026_03_11_043514_add_contrato_id_to_serie_vales_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::table('serie_vales', function (Blueprint $table) {
+        if (!Schema::hasColumn('serie_vales', 'contrato_id')) {
+            $table->foreignId('contrato_id')
+                ->nullable()
+                ->after('id') // La ponemos al inicio para que sea fácil de ver
+                ->constrained('contrato_combustibles')
+                ->nullOnDelete();
+        }
+    });
+}
+
+public function down(): void
+{
+    Schema::table('serie_vales', function (Blueprint $table) {
+        $table->dropConstrainedForeignId('contrato_id');
+    });
+}
+};
