b3bc665782cc904250ea4dcd169b348ab7d0f18f
Steven Rivera <steven.melendez001@gmail.com>
2026-03-11 14:58:43 -0600

    add: ReporteDistribucionVales4

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Maatwebsite\Excel\Facades\Excel;
 
 use App\Http\Controllers\Reportes\ReporteControlMensualCombustibleController;
 
-
+use App\Http\Controllers\Reportes\ReporteDistribucionValesCombustibleController; // Asegúrate de que el controlador exista
 use App\Http\Controllers\Reportes\ReporteFlotaVehicularController;
 
 
@@ -200,4 +200,7 @@ Route::get('/reportes/flota-vehicular/pdf', [ReporteFlotaVehicularController::cl
     ->name('reportes.flota-vehicular.pdf');
 
 Route::get('/reportes/control-mensual-combustible/pdf', [ReporteControlMensualCombustibleController::class, 'pdf'])
-    ->name('reportes.control-mensual-combustible.pdf');    
+    ->name('reportes.control-mensual-combustible.pdf'); 
+
+Route::get('/reportes/distribucion-vales/pdf', [ReporteDistribucionValesCombustibleController::class, 'pdf'])
+        ->name('reportes.distribucion-vales.pdf');   
