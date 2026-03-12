cc98eea74230e928a55c3af71af794a8899e3f4f
Steven Rivera <steven.melendez001@gmail.com>
2026-03-11 19:10:37 -0600

    add: Recepcion & Entrega Vehiculos Reporte

--- app/Filament/Resources/RecepcionEntregaVehiculoResource/Pages/CreateRecepcionEntregaVehiculo.php
+++ app/Filament/Resources/RecepcionEntregaVehiculoResource/Pages/CreateRecepcionEntregaVehiculo.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\RecepcionEntregaVehiculoResource\Pages;
 
 use App\Filament\Resources\RecepcionEntregaVehiculoResource;
-use Filament\Actions;
-use Filament\Resources\Pages\EditRecord;
+use Filament\Resources\Pages\CreateRecord;
 
-class EditRecepcionEntregaVehiculo extends EditRecord
+class CreateRecepcionEntregaVehiculo extends CreateRecord
 {
     protected static string $resource = RecepcionEntregaVehiculoResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\DeleteAction::make(),
-        ];
-    }
 }
