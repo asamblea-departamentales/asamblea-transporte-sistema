5b4f84cf1698b47ccc87222a624a8275e4a30da5
Steven Rivera <steven.melendez001@gmail.com>
2026-03-14 13:36:57 -0600

    add: Agregar nuevos roles3

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -9,23 +9,6 @@ class RolesSeeder extends Seeder
 {
     public function run(): void
     {
-        // Forzamos el guard_name a 'web' para que Filament los vea
-        Role::create(['name' => 'super_admin', 'guard_name' => 'web']);
-        Role::create(['name' => 'solicitante', 'guard_name' => 'web']);
-        Role::create(['name' => 'jefe', 'guard_name' => 'web']);
-        Role::create(['name' => 'ti', 'guard_name' => 'web']);
-    }
-}<?php
-
-namespace Database\Seeders;
-
-use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
-
-class RolesSeeder extends Seeder
-{
-    public function run(): void
-    {
         Role::firstOrCreate(['name' => 'super_admin', 'guard_name' => 'web']);
         Role::firstOrCreate(['name' => 'solicitante', 'guard_name' => 'web']);
         Role::firstOrCreate(['name' => 'jefe', 'guard_name' => 'web']);
