d742d045b6e5cfcd9d9a4b0048dc073188082f10
Steven Rivera <steven.melendez001@gmail.com>
2026-03-17 13:13:30 -0600

    add: Proceso de Despacho de Vehiculo#3

--- app/Models/SolicitudTransporte.php
+++ app/Models/SolicitudTransporte.php
@@ -6,6 +6,8 @@ use App\Domain\Solicitudes\Enums\EstadoSolicitudEnum;
 use App\Domain\Solicitudes\Enums\PrioridadSolicitudEnum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class SolicitudTransporte extends Model
 {
