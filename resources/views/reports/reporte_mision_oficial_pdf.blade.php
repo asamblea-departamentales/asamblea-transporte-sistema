8068e007e1312a1a69a2dbf38946bc4edb4e67cf
Steven Rivera <steven.melendez001@gmail.com>
2026-03-17 11:31:45 -0600

    change: Tamaño letra Mision oficial reporte

--- resources/views/reports/reporte_mision_oficial_pdf.blade.php
+++ resources/views/reports/reporte_mision_oficial_pdf.blade.php
@@ -6,7 +6,7 @@
     <style>
         body {
             font-family: 'DejaVu Sans', sans-serif;
-            font-size: 11px;
+            font-size: 18px;
             color: #111827;
             margin: 30px;
         }
