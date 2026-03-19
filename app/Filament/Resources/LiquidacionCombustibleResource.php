729e6fa89c48b8bcb281741b4f17b9d99f62eab4
Steven Rivera <steven.melendez001@gmail.com>
2026-03-19 15:42:41 -0600

    add: Proceso de liquidaciones unificado

--- app/Filament/Resources/LiquidacionCombustibleResource.php
+++ app/Filament/Resources/LiquidacionCombustibleResource.php
@@ -18,6 +18,8 @@ class LiquidacionCombustibleResource extends Resource
 {
     protected static ?string $model = SolicitudCombustible::class;
 
+    protected static bool $shouldRegisterNavigation = false;
+
     protected static ?string $navigationGroup = 'Liquidación';
     protected static ?string $navigationLabel = 'Panel de Liquidación';
     protected static ?string $navigationIcon = 'heroicon-o-check-badge';
