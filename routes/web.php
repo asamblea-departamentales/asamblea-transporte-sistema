a1058205384ed2c75ca9fde560392f931ccce2cb
Steven Rivera <steven.melendez001@gmail.com>
2026-03-19 11:43:28 -0600

    add: Reporte + Logica Liquidaciones Combustible#2

--- routes/web.php
+++ routes/web.php
@@ -227,7 +227,7 @@ Route::get('/reportes/recepcion-entrega-vehiculo/pdf', [ReporteRecepcionEntregaV
 
 Route::get('/liquidacion/{record}/pdf', function (SolicitudCombustible $record) {
     // Nota que ahora usamos Pdf (con minúsculas según el estandar moderno)
-    $pdf = Pdf::loadView('reports.liquidacion', [
+    $pdf = Pdf::loadView('reports.liquidacion_pdf', [
         'solicitud' => $record,
         'liquidacion' => $record->liquidacion,
     ]);
