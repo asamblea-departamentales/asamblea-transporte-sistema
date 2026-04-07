36aab0d0934cf8dc2144fdeca0de72d2e1e2eb4e
Steven Rivera <steven.melendez001@gmail.com>
2026-04-07 15:04:46 -0600

    change: Resource/View Motorista para mostrar incapacidades #4

--- app/Domain/Solicitudes/Services/MotoristaService.php
+++ app/Domain/Solicitudes/Services/MotoristaService.php
@@ -4,25 +4,23 @@ namespace App\Domain\Solicitudes\Services;
 
 use App\Models\Motorista;
 use App\Models\MotoristaEstado;
-use Carbon\Carbon; // Es mejor usar Carbon para Laravel
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class MotoristaService
 {
     public function cambiarEstado(Motorista $motorista, bool $activo, ?string $motivo = null, ?string $archivo = null): MotoristaEstado
-{
-
+    {
         return DB::transaction(function () use ($motorista, $activo, $motivo, $archivo) {
-            // Cerrar el estado anterior si existe
-           $motorista->estados()->whereNull('fecha_fin')->update(['fecha_fin' => Carbon::now()]);
+            $motorista->estados()->whereNull('fecha_fin')->update(['fecha_fin' => Carbon::now()]);
 
-            // Crear un nuevo estado
-            $motorista->estados()->create([
+            return $motorista->estados()->create([
                 'activo' => $activo,
                 'motivo' => $motivo,
-                'archivo' => $archivo, // Guardar la ruta del archivo si se proporcionó
+                'archivo' => $archivo,
                 'fecha_inicio' => Carbon::now(),
                 'user_id' => auth()->id(),
             ]);
-        }
-
+        });
+    }
 }
