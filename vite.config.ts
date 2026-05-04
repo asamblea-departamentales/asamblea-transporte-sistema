6d125588a098e22d5864f0d76cc081d6ee0a43bc
Carlos <carlos.herrera23@itca.edu.sv>
2026-05-04 09:41:23 -0600

    Actualizacion Dash1.13

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
             src: "/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "any",
           },
         ],
       },
