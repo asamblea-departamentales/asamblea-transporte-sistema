437ac0e2d23a07b0095ed83b94e5adf553b51fc4
Carlos <carlos.herrera23@itca.edu.sv>
2026-07-07 20:24:36 -0600

    Actualizacion de changed de Incapacidad11.1

--- src/disponibilidad/IncapacidadPage.tsx
+++ src/disponibilidad/IncapacidadPage.tsx
@@ -57,7 +57,7 @@ export default function IncapacidadPage() {
             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z" />
           </svg>
-          Cargando estado...
+          Cargando estado....
         </div>
       </div>
     );
